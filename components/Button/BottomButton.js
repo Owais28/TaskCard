6ff6b09b7d0e845813051f3af524ec31b6037428
Owais Athar <owaisathar2882@gmail.com>
2022-12-05 20:25:47 +0530

    init

---
+++ components/Button/BottomButton.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Button, Box } from '@mui/material'
+
+export const BottomButton = ({children, selected=false, color, py, colorIfSelected}) => {
+  return (
+    <Box sx={{
+      flex: 1,
+      height: '100%'
+    }}>
+      <Button color={
+        selected ? colorIfSelected : color
+        // 'notSelected'
+      } sx={{
+        width: '100%',
+        py: 2,
+        height: '100%',
+        borderRadius: 0
+      }}>
+        {
+          children
+        }
+      </Button>
+    </Box>
+  )
+}
