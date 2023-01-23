30c24927e499111b15d2973c38a8743a6314b22b
Owais Athar <owaisathar2882@gmail.com>
2023-01-23 19:38:42 +0530

    feat: add modal

--- src/components/Navigation/BottomMenu.jsx
+++ src/components/Navigation/BottomMenu.jsx
@@ -4,10 +4,11 @@ import {
   Paper,
   Button,
   SwipeableDrawer,
-  Popover,
   Typography,
+  Modal,
+  Fade,
   Stack,
-  Popper,
+  Icon,
 } from "@mui/material";
 import {
   AddCircleRounded,
@@ -16,18 +17,35 @@ import {
   SettingsRounded,
 } from "@mui/icons-material";
 import FormatListBulletedRoundedIcon from "@mui/icons-material/FormatListBulletedRounded";
+import ListAltRoundedIcon from "@mui/icons-material/ListAltRounded";
+// import { AiOutlineProject } from "react-icons/ai";
 
 import { BottomButton } from "../Button/BottomButton";
 import { useSwiper } from "swiper/react";
 
-const styles = {
-  root: {
-    color: "green",
-    "&$selected": {
-      color: "red",
-    },
-  },
-  selected: {},
+// const styles = {
+//   root: {
+//     color: "green",
+//     "&$selected": {
+//       color: "red",
+//     },
+//   },
+//   selected: {},
+// };
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  bottom: "100%",
+  transform: "translate(-50%, -50%)",
+  width: "100vw",
+  height: "100vh",
+  bgcolor: "transparent",
+  // border: "1px solid whitesmoke",
+  boxShadow: "none",
+  // borderRadius: "lg",
+  // p: 4,
 };
 
 export const BottomMenu = () => {
@@ -42,8 +60,9 @@ export const BottomMenu = () => {
     settings: false,
   });
 
-  // console.log(selectedButton)
-  // console.log(swiper)
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   // function that run un SlideChange event
   swiper.on("slideChange", (swiper) => handleSlideChange(swiper.activeIndex));
@@ -120,22 +139,22 @@ export const BottomMenu = () => {
     }
   };
 
-  const [anchorEl, setAnchorEl] = useState(null);
+  // const [anchorEl, setAnchorEl] = useState(null);
   // const [open, setOpen] = useState(false);
 
-  const handlePopperClick = (event) => {
-    setAnchorEl(anchorEl ? null : event.currentTarget);
-  };
+  // const handlePopperClick = (event) => {
+  //   setAnchorEl(anchorEl ? null : event.currentTarget);
+  // };
 
-  const handlePopperClose = (event) => {
-    setAnchorEl(null);
-  };
+  // const handlePopperClose = (event) => {
+  //   setAnchorEl(null);
+  // };
 
-  const open = Boolean(anchorEl);
-  const id = open ? "simple-popover" : undefined;
+  // const open = Boolean(anchorEl);
+  // const id = open ? "simple-popover" : undefined;
 
   return (
-    <Box>
+    <Box position={"relative"}>
       <Paper
         sx={{
           position: "fixed",
@@ -195,12 +214,11 @@ export const BottomMenu = () => {
               height: "100%",
               borderRadius: 0,
             }}
-            aria-describedby={id}
-            onClick={handlePopperClick}
+            onClick={handleOpen}
           >
             <AddCircleRounded fontSize="large" />
           </Button>
-          <Popper
+          {/* <Popper
             id={id}
             open={open}
             anchorEl={anchorEl}
@@ -225,7 +243,72 @@ export const BottomMenu = () => {
                 <Typography>Project</Typography>
               </Box>
             </Stack>
-          </Popper>
+          </Popper> */}
+          <Modal
+            closeAfterTransition
+            sx={style}
+            open={open}
+            onClose={handleClose}
+            // aria-labelledby="modal-modal-title"
+            // aria-describedby="modal-modal-description"
+            // BackdropComponent={Backdrop}
+            // BackdropProps={{
+            //   timeout: 500,
+            // }}
+          >
+            <Fade in={open}>
+              <Box
+                position={"absolute"}
+                top={"40vh"}
+                left={"25%"}
+                bgcolor={"white"}
+                width={"55%"}
+                borderRadius={3}
+                p={2}
+                // boxShadow={"lg"}
+              >
+                {/* <Button fullWidth> */}
+                <Stack
+                  direction={"row"}
+                  alignItems={"center"}
+                  justifyContent={"space-between"}
+                >
+                  <Typography
+                    color={"rgb(0,0,0,0.7)"}
+                    variant="body2"
+                    // fontSize={"13px"}
+                    // fontFamily={"Rubik"}
+                    // color={"rgb(0,0,0,0.6)"}
+                  >
+                    Project
+                  </Typography>
+                  <Icon>
+                    <ListAltRoundedIcon
+                      fontSize="small"
+                      sx={{ color: "rgb(0,0,0,0.7)" }}
+                    />
+                  </Icon>
+                </Stack>
+                {/* </Button> */}
+                <Stack
+                  direction={"row"}
+                  alignItems={"center"}
+                  justifyContent={"space-between"}
+                  sx={{ mt: 2 }}
+                >
+                  <Typography color={"rgb(0,0,0,0.7)"} variant="body2">
+                    Task
+                  </Typography>
+                  <Icon>
+                    <FormatListBulletedRoundedIcon
+                      fontSize="small"
+                      sx={{ color: "rgb(0,0,0,0.7)" }}
+                    />
+                  </Icon>
+                </Stack>
+              </Box>
+            </Fade>
+          </Modal>
         </Box>
 
         {/* Messages Button */}
