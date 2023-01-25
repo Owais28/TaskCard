47971848729460674ba56e34d633b5c18c18b273
Owais Athar <owaisathar2882@gmail.com>
2023-01-25 16:32:06 +0530

    structure updated

--- src/data/router.jsx
+++ src/data/router.jsx
@@ -1,3 +1,35 @@
+import Root from "../pages/Root";
+import { lazy } from "react";
+import { createBrowserRouter } from "react-router-dom";
+import { Suspense as ReactSuspense } from "react";
+import { CircularProgress } from "@mui/material";
+
+const Home = lazy(() => import("../pages/HomePage"));
+const SeeProject = lazy(() => import("../pages/SeeProject"));
+const ProjectTask = lazy(() => import("../pages/ProjectTask"));
+const Notification = lazy(() => import("../pages/NotificationPage"));
+
+// suspense component
+const Suspense = ({ children }) => (
+  <ReactSuspense
+    fallback={
+      <div
+        style={{
+          display: "flex",
+          height: "100vh",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <CircularProgress />
+      </div>
+    }
+  >
+    {children}
+  </ReactSuspense>
+);
+
 export const router = createBrowserRouter([
   {
     path: "/",
