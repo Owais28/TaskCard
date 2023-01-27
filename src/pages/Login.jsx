15db739bf5c2a3ffe0d6828cf97768725d5b8f34
Faisal <biddafaisal@gmail.com>
2023-01-27 22:14:01 +0530

    Added signup page and error page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavigationalAppBar } from '../components/NavigationalAppBar'
 import Button from '@mui/material/Button';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -7,6 +7,8 @@ import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Visibility from "@mui/icons-material/Visibility";
 import { MobileContainer } from "../components/Container/MobileContainer";
 import { Divider, InputAdornment, TextField } from '@mui/material';
 import { AccountCircle, Apple, CircleOutlined, Facebook, Google } from '@mui/icons-material';
