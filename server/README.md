83b27e0de043fc9b4169f80b2f81439a6eafee0d
Owais Athar <owaisathar2882@gmail.com>
2023-01-31 21:20:27 +0530

    feat: added Task API

---
+++ server/README.md
@@ -0,0 +1,23 @@
+# TaskCard - Server
+
+## Features
+
+A user can:
+
+1. create a task
+2. update a task title
+3. update the task due date
+4. update the task status (todo, in-progress, done)
+5. delete a task
+6. create a new project
+7. delete a project
+8. update a project title
+9. update project status
+10. update project due date
+11. create a task in a project
+12. update a task in project
+13. delete the project
+14. delete a specific task from the project
+15. create his profile
+16. update his profile
+17. delete his profile
--- server/api/task/TaskController.js
+++ server/api/task/TaskController.js
@@ -1,20 +1,112 @@
 const TaskModel = require('../../models/taskModel')
 
 const TaskController = {
+  // GET
   // find all tasks
   all: async (req, res) => {
     const tasks = await TaskModel.find()
     res.json({ tasks })
   },
 
-  // find specific task by task id
+  // find all tasks from a specific project
+  allInProject: async (req, res) => {
+    const { projectId } = req.params
+    const tasks = await TaskModel.find({ project: projectId })
+    res.json(tasks)
+  },
+
+  // find a specific task by <taskId>
   one: async (req, res) => {
     const task = await TaskModel.findById(req.params.id)
     res.json({ task })
   },
 
+
+  // POST
+  // create a task
+  create: async (req, res, next) => {
+    const { title, dueDate, } = req.body
+    try {
+      const task = await TaskModel.create({ title, dueDate, status: "todo" })
+    } catch (error) {
+      next(error)
+    }
+  },
+
+  // create a task in a project
+  createInProject: async (req, res, next) => {
+    const { title, dueDate } = req.body
+    const { projectId } = req.params
+
+    try {
+      const task = await TaskModel.create({ title, dueDate, project: projectId })
+      res.json({ message: "Task has been created successfully.", task })
+    } catch (error) {
+      next(error)
+    }
+  },
+
+  // PUT
   // update task 
   update: async (req, res) => {
-    if (req.params.query)
+
+    const { id } = req.body
+    const { q } = req.query
+
+    // if update request is for title
+    if (q === "title") {
+      const task = await TaskModel.findByIdAndUpdate(id, { title: req.body.title })
+      res.json({ message: "Task has been updated successfully.", task })
+    }
+
+    // if update request is for due date
+    if (q === "date") {
+      const task = await TaskModel.findByIdAndUpdate(id, { dueDate: req.body.dueDate })
+      res.json({ message: "Task has been updated successfully.", task })
+    }
+
+  },
+
+  // update a task in a project
+  updateInProject: async (req, res) => {
+
+    const { projectId, taskId } = req.params
+    const { q } = req.query // query specifies the update type
+
+    // if request is for updating title of the task
+    if (q === "title") {
+      const task = await TaskModel.findOneAndUpdate({ _id: taskId, project: projectId }, { title: req.body.title })
+      res.json({ message: "Task has been updated successfully.", task })
+    }
+
+    // if request is for updating due date of the task
+    if (q === "date") {
+      const task = await TaskModel.findOneAndUpdate({ _id, taskId, project: projectId }, { dueDate: req.body.dueDate })
+      res.json({ message: "Task has been updated successfully.", task })
+    }
+
+    // if request is for updating status of the task
+    if (q === "status") {
+      const task = await TaskModel.findOneAndUpdate({ _id, taskId, project: projectId }, { status: req.body.status })
+      res.json({ message: "Task has been updated successfully.", task })
+    }
+  },
+
+  // DELETE
+  delete: async (req, res) => {
+    const { id } = req.params
+    const result = await TaskModel.findByIdAndDelete(id)
+    res.json({ message: "Task has been deleted successfully." })
+  },
+
+  deletInProject: async (req, res) => {
+
+    const { projectId, taskId } = req.params
+
+    const result = await TaskModel.findOneAndDelete({ _id: taskId, project: projectId })
+    res.json({ message: "Task has been deleted successfully." })
   }
 }
+
+module.exports = TaskController
+
