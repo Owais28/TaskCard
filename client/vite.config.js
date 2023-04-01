b85530ec292f2e13e8760d444f5c3d1b57632d58
Owais Athar <owaisathar2882@gmail.com>
2023-04-01 16:40:28 +0530

    updated manifest

--- client/vite.config.js
+++ client/vite.config.js
@@ -17,6 +17,9 @@ export default defineConfig({
         short_name: "TaskCard",
         description: "an app to manage your life and work",
         start_url: '/',
+        display: "standalone",
+        display_override: ["window-controls-overlay"],
+        lang: 'en-us',
         icons:
           [{
             src: "/android-chrome-192x192.png",
@@ -24,15 +27,63 @@ export default defineConfig({
             type: "image/png"
           },
           {
-            src: "/android-chrome-512x512.png",
+            src: "/android-chrome-152x152.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: 'any maskable',
-          }],
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        display: "standalone",
-        lang: 'en',
+          },
+          {
+            src: "/android-launchericon-144-144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "/android-launchericon-96-96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "/android-launchericon-72-72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "/android-launchericon-48-48.png",
+            sizes: "48x48",
+            type: "image/png",
+          },
+          {
+            src: "/maskable_icon_x48.png",
+            sizes: "48x48",
+            type: "image/png",
+            purpose: "maskable"
+          },
+          {
+            src: "/maskable_icon_x72.png",
+            sizes: "72x72",
+            type: "image/png",
+            purpose: "maskable"
+          },
+          {
+            src: "/maskable_icon_x96.png",
+            sizes: "96x96",
+            type: "image/png",
+            purpose: "maskable"
+          },
+          {
+            src: "/maskable_icon_x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable"
+          },
+          {
+            src: "/maskable_icon_x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
+          },
+          ],
+        // theme_color: "#ffffff",
+        // background_color: "#ffffff",
         scope: '/',
       }
     })
